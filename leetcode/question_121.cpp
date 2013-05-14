3e46aa40810f348df767fa584a4f3c3eb556b406
Mingliang LIU <liuml07@gmail.com>
2013-05-15 00:09:44 +0800

    Refine the question 121 of leetcode
    No need to keep track of the diffs vector. This can save a lot of memory
    usage.

--- leetcode/question_121.cpp
+++ leetcode/question_121.cpp
@@ -3,23 +3,15 @@ public:
     int maxProfit(vector<int> &prices) {
         if (prices.size() == 0)
             return 0;
-            
-        vector<int> diffs;
-        
-        int p = prices[0];
-        for (auto i = prices.begin(); i != prices.end(); i++) {
-            diffs.push_back(*i - p);
-            p = *i;
-        }
-        
+         
         int max = 0;
-        int max_sofar = 0;
-        for (auto i = diffs.begin(); i != diffs.end(); i++) {
-            max_sofar += *i;
+        int max_sofar = 0;           
+        int p = prices[0];
+        for (auto i = prices.begin(); i != prices.end(); p = *i, i++) {
+            max_sofar += *i - p;
             if (max_sofar < 0)
                 max_sofar = 0;
-                
-            if (max_sofar > max)
+            else if (max_sofar > max)
                 max = max_sofar;
         }
         
