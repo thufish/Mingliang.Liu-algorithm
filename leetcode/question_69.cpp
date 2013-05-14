d481f1489e4399c49a688a046ecfb0c758a47fe7
Mingliang LIU <liuml07@gmail.com>
2013-05-15 01:01:28 +0800

    Add sqrt of leetcode

---
+++ leetcode/question_69.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sqrt(int x) {
+        double y,z(1.0);
+        while(int(y)-int(z)){
+            y=z;
+            z=double(x)/2/y+y/2;
+        }
+        return int(y);
+    }
+};
