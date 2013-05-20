e06a5fbca8acd680dc037b44f27779c353e0fe39
Mingliang LIU <liuml07@gmail.com>
2013-05-20 11:53:39 +0800

    Add the recursive version solution to question 78
    I thought the recursive was very easy to implement. I was wrong. Note
    the return cases in the recursive routine.

--- leetcode/question_78.cpp
+++ leetcode/question_78.cpp
@@ -1,14 +1,21 @@
+/** Given a set of distinct integers, S, return all possible subsets.
+    Note:
+    - Elements in a subset must be in non-descending order.
+    - The solution set must not contain duplicate subsets.
+*/
+
 #include <algorithm>
 #include <cmath>
 #include <vector>
 
 using namespace std;
 
+/// This solution uses the bit vector to select the elements
+/// For n numbers in S, there are 2^n bit vector elements; thus we should
+/// select the number if the bit is 1 and ignore the number if the bit is 0
 class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         vector<int> s(S);
         sort(s.begin(), s.end());
         
@@ -30,5 +37,38 @@ public:
         }
         
         return answer;
+    }
+};
+
+/// This solution uses the recursive method to select the elements
+/// The final trick here is when to add the current answer (index == S.size())
+class Solution2 {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int>> subsets;
+        if (S.size() == 0)
+            return subsets;
+
+        sort(S.begin(), S.end());
+        vector<int> subset;
+        subsetsRecursive(subsets, S, subset, 0);
+        
+        return subsets;
+    }
+    
+    void subsetsRecursive(vector<vector<int>> &subsets, vector<int> &S, vector<int> subset, int index) {
+        if (index == S.size()) {
+            // Add the current answer
+            sort(subset.begin(), subset.end());
+            subsets.push_back(subset);
+            return;
+        }
+        
+        // Don't add S[index]
+        subsetsRecursive(subsets, S, subset, index + 1);
+        
+        // Add S[index]
+        subset.push_back(S[index]);
+        subsetsRecursive(subsets, S, subset, index + 1);
     }
 };
