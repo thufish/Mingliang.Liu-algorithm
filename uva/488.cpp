669d5810435d5274d5c041b030b078f0c3ffe410
Mingliang LIU <liuml07@gmail.com>
2013-05-23 01:31:13 +0800

    Add solution to problem 488 of uva

---
+++ uva/488.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int times;
+    cin >> times;
+
+    for (int t = 0; t < times; t++) {
+        int A, F;
+        getchar();
+        string tmp;
+        getline(cin, tmp);
+
+        cin >> A >> F;
+
+        for (int i = 0; i < F; i++) {
+            // print each wave
+            for (int j = 1; j <= A; j++) {
+                for (int k = 0; k < j; k++)
+                    cout << j;
+                cout << endl;
+            }
+            for (int j = A - 1; j >= 1; j--) {
+                for (int k = 0; k < j; k++)
+                    cout << j;
+                cout << endl;
+            }
+     
+            // print the seperate line
+            if (i != F - 1)
+                cout << endl;
+        }
+    }
+    return 0;
+}
