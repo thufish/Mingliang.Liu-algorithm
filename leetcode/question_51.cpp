4ad092adb22ee6187036e3859bafdfde09aa38d9
Mingliang LIU <liuml07@gmail.com>
2013-05-23 23:17:53 +0800

    Add solution3 for question 51 of leetcode

--- leetcode/question_51.cpp
+++ leetcode/question_51.cpp
@@ -4,7 +4,13 @@
 
 using namespace std;
 
-class Solution {
+/**
+ * This solution permutes the colums by hand and prunes them once they
+ * conflict. Thus this method is much more efficient than the first one.
+ *
+ * This solution passes the leetcode when n == 9
+ */
+class Solution2 {
 public:
     vector<vector<string> > solveNQueens(int n) {
         vector<vector<string>> ans;
@@ -54,8 +60,57 @@ public:
     }
 };
 
-int main() {
-    Solution S;
-    auto ans = S.solveNQueens(1);
-    return 0;
-}
+/** In this approach, we check the candidate in constant time and prune them
+ * as soon as possible
+ *
+ * This can pass the leetcode when n is up to 9
+ */
+class Solution3 {
+public:
+    vector<vector<string> > solveNQueens(int n) {
+        vector<vector<string>> ans;
+        if (n == 2 || n == 3)
+            return ans;
+            
+        vector<bool> taken(n, false);
+        vector<bool> taken_left(2 * n, false);
+        vector<bool> taken_right(2 * n, false);
+        vector<int> cols(n, -1);
+        solveNQueens(ans, cols, taken, taken_left, taken_right, 0, n);
+        
+        return ans;
+    }
+    
+    void solveNQueens(vector<vector<string>> &ans,
+                      vector<int> &cols,
+                      vector<bool> &taken,
+                      vector<bool> &taken_left,
+                      vector<bool> &taken_right,
+                      int row,
+                      int n) {
+        // found one candidate
+        if (row == n) {
+            vector<string> a;
+            for (int i = 0; i < n; i++) {
+                string str(n, '.');
+                str[cols[i]] = 'Q';
+                a.push_back(str);
+            }
+            ans.push_back(a);
+        } else {
+            for (int col = 0; col < n; col++) {
+                if (!taken[col] && !taken_right[row + col] && !taken_left[n + col - row]) {
+                    taken[col] = true;
+                    taken_right[row + col] = true;
+                    taken_left[n + col - row] = true;
+                    cols[row] = col;
+                    solveNQueens(ans, cols, taken, taken_left, taken_right, row + 1, n);
+                    
+                    taken[col] = false;
+                    taken_right[row + col] = false;
+                    taken_left[n + col - row] = false;
+                }
+            }
+        }
+    }
+};
