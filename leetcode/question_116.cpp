357190f2e786ed8dcd909135a4603adefd498012
Mingliang LIU <liuml07@gmail.com>
2013-06-06 18:07:16 +0800

    Add recursive solution to 116 of leetcode

--- leetcode/question_116.cpp
+++ leetcode/question_116.cpp
@@ -6,19 +6,37 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
-class Solution {
+
+/** The iterative version */
+class Solution1 {
 public:
     void connect(TreeLinkNode *root) {
         TreeLinkNode *p, *pp;
         pp = root;
-        while (pp) {
+        while (pp && pp->left && pp->right) { // NOTE: Ignore the leaf nodes
             p = pp;
             pp = p->left;
-            while (p && p->left && p->right) { // NOTE: Ignore the leaf nodes
+            while (p) {
                 p->left->next = p->right;
                 p->right->next = p->next ? p->next->left : NULL;
                 p = p->next;
             }
         }
+    }
+};
+
+
+/** The recursive version */
+class Solution2 {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root || (!root->left && !root->right))
+            return;
+        root->left->next = root->right;
+        if (root->next) 
+            root->right->next = root->next->left;
+
+        connect(root->left);
+        connect(root->right);
     }
 };
