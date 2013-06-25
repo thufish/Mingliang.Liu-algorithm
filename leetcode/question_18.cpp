630895cb3ccb5f5228b3e749b1b7cc3b3e8f7742
Mingliang LIU <liuml07@gmail.com>
2013-06-25 11:03:15 +0800

    Add tangzecn's solution to 4-sum

--- leetcode/question_18.cpp
+++ leetcode/question_18.cpp
@@ -5,7 +5,10 @@
 
 using namespace std;
 
-class Solution {
+/**
+ * Use the hash map to hold the two sums
+ */
+class Solution1 {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
         unordered_map<int, vector<pair<int, int>>> sums;
@@ -32,6 +35,38 @@ public:
                     }
             }
         }
+        
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
+
+/**
+ * Just like the 3-sum problem
+ */
+class Solution2 {
+public:
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+        /** sort first */
+        sort(num.begin(), num.end());
+
+        /* we can also use the vec.erase(unique(), end) method here */
+        set<vector<int>> ans;
+        for (int i = 0; i < num.size(); ++i)
+            for (int j = i + 1; j < num.size(); ++j) {
+                int k = j + 1;
+                int m = num.size() - 1;
+                while (k < m) {
+                    if (num[i] + num[j] + num[k] + num[m] > target)
+                        --m;
+                    else if (num[i] + num[j] + num[k] + num[m] < target)
+                        ++k;
+                    else {
+                        ans.insert({num[i], num[j], num[k], num[m]});
+                        --m;
+                        ++k;
+                    }
+                }
+            }
         
         return vector<vector<int>>(ans.begin(), ans.end());
     }
