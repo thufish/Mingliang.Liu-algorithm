0b2650ac546d6bb43143c701b5adee16b92ee822
Mingliang LIU <liuml07@gmail.com>
2013-07-02 10:30:26 +0800

    Add improved solution for question 62 of leetcode

--- leetcode/question_62.cpp
+++ leetcode/question_62.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int uniquePaths(int m, int n) {
         vector<int> paths(n, 1);
@@ -11,5 +11,20 @@ public:
         }
         
         return paths[n - 1];
+    }
+};
+
+/** Actuall, only one array is needed here */
+class Solution2 {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                dp[j+1] = dp[j]+dp[j+1];
+            }
+        }
+        return dp.back();
     }
 };
