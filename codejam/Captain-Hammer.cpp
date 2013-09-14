e20ffdb5b4b73f1b19e9801cafeb1cf82723d7d5
Mingliang LIU <liuml07@gmail.com>
2013-09-15 00:27:24 +0800

    Fix the NaN error

--- codejam/Captain-Hammer.cpp
+++ codejam/Captain-Hammer.cpp
@@ -13,6 +13,8 @@ int main() {
         unsigned V, D;
         cin >> V >> D;
         double S = D * 9.8 / V / V;
+        S = S > 1 ? 1 : S;
+        S = S < 0 ? 0 : S;
         double ans = asin(S) * 180.0 / M_PI / 2;
 
         cout << "Case #" << t << ": ";
