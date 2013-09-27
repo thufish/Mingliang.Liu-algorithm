bebda282d2afc72b94ba7c54f1785842dd9c6eea
Mingliang LIU <liuml07@gmail.com>
2013-09-27 10:36:02 +0800

    Add solution to search-in-rotated-sorted-array

--- leetcode/search-in-rotated-sorted-array.cpp
+++ leetcode/search-in-rotated-sorted-array.cpp
@@ -12,11 +12,13 @@ public:
             if (A[mid] == target)
                 return mid;
             else if (A[mid] > target) {
+                //if (A[mid] > A[right] && A[right] >= target)
                 if (A[mid] > A[right] && target < A[left])
                     left = mid + 1;
                 else
                     right = mid - 1;
             } else {
+                //if (A[mid]  < A[left] && A[left] <= target)
                 if (A[mid] < A[left] && target > A[right])
                     right = mid - 1;
                 else
