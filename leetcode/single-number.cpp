852dbcab2463a0e55f4c62aba0b925cdfbaabd42
Mingliang LIU <liuml07@gmail.com>
2013-10-15 16:45:31 +0800

    Add solution to single number of leetcode

---
+++ leetcode/single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        if (n < 1)
+            return 0;
+
+        int num = A[0];
+        for (int i = 1; i < n; i++)
+            num ^= A[i];
+            
+        return num;
+    }
+};
