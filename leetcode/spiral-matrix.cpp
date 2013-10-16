17017e97c8512189cce390a89194e9cf02fba5ac
Mingliang LIU <liuml07@gmail.com>
2013-10-16 23:21:17 +0800

    Fix the solution to spiral matrix problem

--- leetcode/spiral-matrix.cpp
+++ leetcode/spiral-matrix.cpp
@@ -1,20 +1,29 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int> > &matrix) {
         vector<int> ans;
+        if (matrix.size() < 1)
+            return ans;
+        
+        int m = matrix.size(), n = matrix[0].size();
 
-        for (int i = 0; i < matrix.size() / 2; i++) {
+        for (int i = 0; i < min((m + 1) / 2, (n + 1) / 2); i++) {
             // top
-            for (int j = i; j < matrix[0].size() - i; j++)
+            for (int j = i; j < n - i; j++)
                 ans.push_back(matrix[i][j]);
             // right
-            for (int j = i + 1; j < matrix.size() - i; j++)
-                ans.push_back(matrix[j][i]);
+            for (int j = i + 1; j < m - i; j++)
+                ans.push_back(matrix[j][n - 1 - i]);
             // bottom
-            for (int j = matrix[0].size() - i - 2; j >= i; j--)
-                ans.push_back(matrix[i][j]);
+            for (int j = n - i - 2; m - 1 - i > i && j >= i; j--)
+                ans.push_back(matrix[m - 1 - i][j]);
             // left
-            for (int j = matrix.size() - i - 2; j > i; j--)
+            for (int j = m - i - 2; n - 1 - i > i && j > i; j--)
                 ans.push_back(matrix[j][i]);
         }
         
