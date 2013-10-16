764441019412e76cd42ea5d48c0bf84dfda1b16a
Mingliang LIU <liuml07@gmail.com>
2013-10-16 22:12:31 +0800

    Refine solution to permutations.cpp

--- leetcode/permutations.cpp
+++ leetcode/permutations.cpp
@@ -1,42 +1,41 @@
-class Solution {
-public:
-    vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int>> ans;
-        vector<int> a;
-        vector<bool> visited(num.size(), false);
-        permuteHelper(num, visited, a, ans);
-        return ans;
-    }
-    
-    void permuteHelper(vector<int> &num, vector<bool> &visited, vector<int> a, vector<vector<int>> &ans) {
-        if (a.size() == num.size()) {
-            ans.push_back(a);
-            return;
-        }
-        for (int i = 0; i < num.size(); i++) {
-            if (visited[i])
-                continue;
+#include <vector>
+#include <algorithm>
 
-            a.push_back(num[i]);
-            visited[i] = true;
-            permuteHelper(num, visited, a, ans);
-            a.pop_back();
-            visited[i] = false;
-        }
-    }
-};
+using namespace std;
 
 /** Cheating
  */
-class Solution2 {
+class Solution1 {
 public:
     vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int>> ans;
+        vector<vector<int> > ans;
         sort(num.begin(), num.end());
         do {
             ans.push_back(vector<int>(num.begin(), num.end()));    
         } while (next_permutation(num.begin(), num.end()));
         
         return ans;
+    }
+};
+
+/* I gave this answer to Li Zhuang when I was interviewed at MSRA.
+ * She said it's not correct. She was wrong.
+ */
+class Solution2 {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> > ans;
+        permuteHelper(num, 0, ans);
+        return ans;
+    }
+    
+    void permuteHelper(vector<int> num, int index, vector<vector<int> > &ans) {
+        if (index == num.size()) {
+            ans.push_back(num);
+        } else for (int i = index; i < num.size(); i++) {
+            swap(num[i], num[index]);
+            permuteHelper(num, index + 1, ans);
+        }
     }
 };
