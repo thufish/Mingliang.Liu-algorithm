a0275227e3f7a542e0b1023054ae08db1b935912
Mingliang LIU <liuml07@gmail.com>
2013-10-21 00:11:31 +0800

    Add solution to longest-consecutive-sequence

---
+++ leetcode/longest-consecutive-sequence.cpp
@@ -0,0 +1,37 @@
+#include <unordered_map>
+
+/** The naive solution using hashing map holding range */
+class Solution1 {
+public:
+    int longestConsecutive(vector<int> &num) {
+        unordered_map<int, int> range;
+        int max_len = 1;
+        
+        for (int i = 0; i < num.size(); ++i) {
+            if (range.find(num[i]) != range.end())
+                continue;
+
+            range[num[i]] = num[i];
+            
+            if (range.find(num[i] - 1) != range.end()) {
+                int min = range[num[i] - 1];
+                int max = num[i];
+                range[num[i]] = min;
+                range[range[num[i] - 1]] = max;
+                if (max - min + 1 > max_len)
+                    max_len = max - min + 1;
+            }
+            
+            if (range.find(num[i] + 1) != range.end()) {
+                int min = range[num[i]];
+                int max = range[num[i] + 1];
+                range[range[num[i] + 1]] = min;
+                range[range[num[i]]] = max;
+                if (max - min + 1 > max_len)
+                    max_len = max - min + 1;
+            }
+        }
+        
+        return max_len;
+    }
+};
